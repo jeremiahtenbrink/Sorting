19f22da8c50f37235c7778e5e572c6ee28b09e3e
jeremiahtenbrink <jeremiahtenbrink@gmail.com>
2019-11-04 12:48:44 -0700

    Finished iterative sorting.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,35 @@
 # TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
+def selection_sort(arr):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1):
-        cur_index = i
-        smallest_index = cur_index
+        smallest_index = i
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
-             
+        for j in range(i + 1, len(arr)):
+            if arr[smallest_index] > arr[j]:
+                smallest_index = j
 
-
-
-        # TO-DO: swap
-
-
-
+        if i != smallest_index:
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    swapped = True
+    for i in range(len(arr) -1):
+        swapped = False
+        for j in range(0, len(arr) - i -1):
+            if arr[j] > arr[j+1]:
+                swapped = True
+                arr[j], arr[j+1] = arr[j +1], arr[j]
 
+        if not swapped:
+            break
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
-
+def count_sort(arr, maximum = -1):
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,30 +1,64 @@
-# STRETCH: implement Linear Search				
-def linear_search(arr, target):
-  
-  # TO-DO: add missing code
+import timeit
 
-   return -1   # not found
+array = []
+for i in range(100000000):
+    array.append(i)
+
+
+# STRETCH: implement Linear Search
+def linear_search(arr = array, target = 50000000):
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
+
+    return -1  # not found
 
 
 # STRETCH: write an iterative implementation of Binary Search 
-def binary_search(arr, target):
+def binary_search(arr = array, target = 50000000):
+    if len(arr) == 0:
+        return -1  # array empty
 
-  if len(arr) == 0:
-    return -1 # array empty
-    
-  low = 0
-  high = len(arr)-1
+    low = 0
+    high = len(arr) - 1
+    not_found = True
+    while not_found:
+        half = (high + low) // 2
+        if arr[half] == target:
+            return half
+        elif low == high:
+            break
+        elif arr[half] < target:
+            low = half + 1
+        elif arr[half] > target:
+            high = half - 1
 
-  # TO-DO: add missing code
-
-  return -1 # not found
+    return -1  # not found
 
 
 # STRETCH: write a recursive implementation of Binary Search 
-def binary_search_recursive(arr, target, low, high):
-  
-  middle = (low+high)//2
+def binary_search_recursive(arr = array, target = 50000000, low = 0,
+                            high = len(array) - 1):
+    middle = (low + high) // 2
 
-  if len(arr) == 0:
-    return -1 # array empty
-  # TO-DO: add missing if/else statements, recursive calls
+    if len(arr) == 0:
+        return -1  # array empty
+
+    elif arr[middle] > target:
+        return binary_search_recursive(arr, target, low,
+                                       middle - 1)
+    elif arr[middle] < target:
+        return binary_search_recursive(arr, target, middle + 1,
+                                       high)
+    else:
+        return middle
+
+
+# elapsed_time = timeit.timeit(linear_search, number = 100) / 100
+# print("linear search: ", elapsed_time)
+
+elapsed_time = timeit.timeit(binary_search, number = 100) / 100
+print("binary search: ", elapsed_time)
+
+elapsed_time = timeit.timeit(binary_search_recursive, number = 100) / 100
+print("binary search recursive: ", elapsed_time)
